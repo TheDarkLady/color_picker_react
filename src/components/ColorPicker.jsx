6ab49f463cba19c9f94fddc6b46a4b325394f770
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-10-11 00:06:14 +0530

    basic functionality added

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 export default function ColorPicker() {
     const [color, setColor] = useState('#fff')
+    useEffect(() => {
+        document.body.style.backgroundColor = color
+    },[color])
   return (
     <>
        <div style={{style:"padding:20px"}}>
         <h1>Choose the color</h1>
-
+        <input type="color" value={color} onChange={(e) => setColor(e.target.value)} style={{width:"100px",height:"50px", borderRadius:"10px"}}/>
       </div>
+      <p>selected color : {color}</p>
     </>
   )
 }
